425466f7d462823ccf8c04f4d141bd7c1d6ea1d5
ishdeep <ishdeepsidhu@gmail.com>
2024-11-05 14:43:24 +0530

    feat: Add define team code

---
+++ frontend/src/YupSchemas/DefineTeamSchema.js
@@ -0,0 +1,19 @@
+import * as yup from 'yup'
+export const DefineTeamSchema = yup.object({
+  team: yup.string().required('Team name is required'),
+  emails: yup
+    .array()
+    .of(
+      yup
+        .string()
+        .required('Email is required')
+        .email('Invalid email')
+        .test(
+          'not-empty',
+          'Email cannot be empty',
+          value => value?.trim() !== '',
+        ),
+    )
+    .min(5, 'At 5 emails are required')
+    .max(9, 'Maximum 9 emails allowed'),
+})

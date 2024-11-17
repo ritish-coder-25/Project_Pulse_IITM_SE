e7c27aa385db3216aa554f204955b8a6bc39bc3c
Parag Seth <21f3001992@ds.study.iitm.ac.in>
2024-11-18 00:37:10 +0530

    fix:userAuthentication

--- backend/apis/user_apis/userAuthentication.py
+++ backend/apis/user_apis/userAuthentication.py
@@ -12,12 +12,15 @@ from flask_jwt_extended import (
     get_jwt_identity,
 )
 from datetime import timedelta
-from flask_bcrypt import bcrypt
+from flask_bcrypt import Bcrypt
+from api_outputs.user_api.TADpending_user_outputs import PendingUserListOutput
+from api_outputs.user_api.TADuser_approval_outputs import UserApprovalOutput
+from api_parsers.TADuser_approval_parsers import ApproveUsersRequest
 
 api_bp_auth = Blueprint(
-    "UserAuthentication-Api",
-    "UserAuthentication",
-    description="Operations for User Registration and Login",
+    "User_APIs",
+    "User_APIs",
+    description="Operations for User Registration, Approval and Login",
 )
 
 
@@ -26,9 +29,9 @@ class RegisterResource(Resource):
     # @jwt_required()
 
     def post(self):
-        """API for user registration"""
+        """API for new user registration"""
         data = request.get_json()
-
+        bcrypt = Bcrypt()
         required_fields = [
             "first_name",
             "last_name",
@@ -89,7 +92,7 @@ class LoginResource(Resource):
         """API for user login"""
         data = request.get_json()
         user = User.query.filter_by(email=data["email"]).first()
-
+        bcrypt = Bcrypt()
         if user and bcrypt.check_password_hash(user.password, data["password"]):
             expires = timedelta(days=90)
             access_token = create_access_token(
@@ -99,3 +102,90 @@ class LoginResource(Resource):
             return jsonified, 200
 
         return jsonify({"message": "Invalid credentials"}), 401
+
+
+@api_bp_auth.route("/api/users/pendusers")
+class PendingUserListResource(Resource):
+    @jwt_required()
+    @api_bp_auth.response(200, PendingUserListOutput)
+    def get(self):
+        """API to get the pending users awaiting authorization for registration."""
+        try:
+            pending_users = User.query.filter_by(approval_status="Inactive").all()
+            result = [
+                {
+                    "id": user.user_id,
+                    "name": f"{user.first_name} {user.last_name}",
+                    "email": user.email,
+                    "role": user.user_type if user.user_type else "Student",
+                }
+                for user in pending_users
+            ]
+            return jsonify(result)
+
+        except Exception as e:
+            return createFatalError(
+                "pending_users_fetch_error", "Error fetching pending users", str(e)
+            )
+
+
+@api_bp_auth.route("/api/users/approve_users")
+class ApproveUsersResource(Resource):
+    @jwt_required()
+    @api_bp_auth.arguments(ApproveUsersRequest)
+    @api_bp_auth.response(200, UserApprovalOutput)
+    def post(self, data):
+        """API for TA to approve the new user registrations (app only)"""
+        try:
+            current_user_id = get_jwt_identity()
+            current_user = User.query.get_or_404(current_user_id)
+            allowed_roles = ["TA", "Admin", "Instructor", "Developer"]
+
+            if current_user.user_type not in allowed_roles:
+                return createError(
+                    "approve_users_permission_denied",
+                    "You do not have permission to approve users",
+                    403,
+                )
+
+            results = []
+            for user_data in data["users"]:
+                user_id = user_data.get("user_id")
+                approval_status = user_data.get("approval_status")
+                user_type = user_data.get("user_type")
+
+                user = User.query.get(user_id)
+                if not user:
+                    results.append({"user_id": user_id, "message": "User not found"})
+                    continue
+                user.user_type = user_type
+                if approval_status == "Approved":
+                    user.approval_status = "Active"
+                elif approval_status == "Declined":
+                    user.approval_status = "Decline"
+                else:
+                    results.append(
+                        {"user_id": user_id, "message": "Invalid approval_status"}
+                    )
+                    continue
+
+                db.session.commit()
+                results.append(
+                    {"user_id": user_id, "message": "User approval processed"}
+                )
+
+            return (
+                jsonify(
+                    {
+                        "message": "User approvals processed successfully",
+                        "results": results,
+                    }
+                ),
+                200,
+            )
+        except Exception as e:
+            return createFatalError(
+                "user_approval_processing_error",
+                "Error processing user approvals",
+                str(e),
+            )

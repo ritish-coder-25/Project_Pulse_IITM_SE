da68d71b910d3d786fc5cceb478bb7ba75ac6473
ankushpotgante <21f1002985@ds.study.iitm.ac.in>
2024-11-22 14:23:44 +0530

    added test cases for TA Teams Dashboard

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from main import app as flask_app
 from main import db as _db
-from models import Project
+from models import Project, User
 
 @pytest.fixture(scope='session')
 def app():
@@ -92,3 +92,58 @@ def auth_headers(app):
 #     return {
 #         'Authorization': f'Bearer {access_token}'
 #     }
+
+
+@pytest.fixture
+def ta_auth_headers(app, db):
+    """Generate authentication headers for an admin user."""
+    try:
+        ta_user = User(
+            first_name="TA",
+            last_name="User",
+            email="ta.user@example.com",
+            password='ta',
+            user_type="TA",
+            approval_status="Active"
+        )
+        db.session.add(ta_user)
+        db.session.commit()
+    
+    except:
+        db.session.rollback()
+        ta_user = User.query.filter_by(email='ta.user@example.com').first()
+
+    if ta_user:
+        access_token = create_access_token(identity=ta_user.user_id)
+        return {
+            'Authorization': f'Bearer {access_token}'
+        }
+    else:
+        print("Error while accessing TA User")
+
+@pytest.fixture
+def student_auth_headers(app, db):
+    """Generate authentication headers for an admin user."""
+
+    try:
+        student_user = User(
+            first_name="Student",
+            last_name="User",
+            email="student.user@example.com",
+            password='student',
+            user_type="Student",
+            approval_status="Active"
+        )
+        db.session.add(student_user)
+        db.session.commit()
+    except:
+        db.session.rollback()
+        student_user = User.query.filter_by(email='student.user@example.com').first()
+
+    if student_user:
+        access_token = create_access_token(identity=student_user.user_id)
+        return {
+            'Authorization': f'Bearer {access_token}'
+        }
+    else:
+        print("Error while accessing Student user")

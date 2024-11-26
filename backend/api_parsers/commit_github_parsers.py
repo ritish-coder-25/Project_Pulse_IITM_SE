512c407834ffee4cd4b8c0eb61d7306ed6eddaad
PRANJAL KAR <74347116+pranjalkar99@users.noreply.github.com>
2024-11-26 21:21:59 +0530

    Refactor CommitQueryParamsSchema to allow None values for repo_owner and repo_name

--- backend/api_parsers/commit_github_parsers.py
+++ backend/api_parsers/commit_github_parsers.py
@@ -3,6 +3,6 @@ from marshmallow import Schema, fields
 class CommitQueryParamsSchema(Schema):
     since = fields.String(required=True, description="Start datetime for commit retrieval in ISO format.")
     until = fields.String(required=True, description="End datetime for commit retrieval in ISO format.")
-    repo_owner = fields.String(required=False, description="Owner of the repository.", nullable=True)
-    repo_name = fields.String(required=False, description="Name of the repository.", nullable=True)
+    repo_owner = fields.String(required=False, description="Owner of the repository.", allow_none=True)
+    repo_name = fields.String(required=False, description="Name of the repository.", allow_none=True)
     team_id = fields.Integer(required=True, description="Team ID for the repository.")
